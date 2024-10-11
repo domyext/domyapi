6bb25c4c632204bb8ed2c851c4712d6c4582a96b
HeroBuxx <me@herobuxx.me>
2024-10-11 22:44:18 +0700

    api: Refactor to run as docker container
    We are not using GCP Functions
    
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
-package domyApi
+package main
 
 import (
-	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	route "github.com/domyid/domyapi/route"
+	"log"
+	"net/http"
 )
 
-func init() {
-	functions.HTTP("WebHook", route.URL)
+func main() {
+	// Handle the WebHook route
+	http.HandleFunc("/webhook", route.URL)
+
+	// Start the server on a specific port, e.g., 8080
+	port := ":8080"
+	log.Printf("Server is starting at http://localhost%s", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
