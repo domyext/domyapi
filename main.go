2c81834cca9d87f6a98f8172c28a0fa7ce6e6a90
HeroBuxx <me@herobuxx.me>
2024-10-11 22:45:45 +0700

    api: Correct used port
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ func main() {
 	http.HandleFunc("/webhook", route.URL)
 
 	// Start the server on a specific port, e.g., 8080
-	port := ":8080"
-	log.Printf("Server is starting at http://localhost%s", port)
+	port := ":80"
+	log.Printf("Serber running on port: ", port)
 	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
